86518f7e5756d469bffdafb10c275be13744f44e
wjc <choiwj98@naver.com>
2026-07-15 14:09:29 +0900

    불필요 코드 정리

--- src/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import ChatbotWidget from './components/chatbot/ChatbotWidget.vue'
-</script>
-
-<template>
-  <HelloWorld />
-  <ChatbotWidget />
-</template>
